cd090420f5781590f7e55cb494f4d8fb17f89238
Justin Toh <tohjustin@hotmail.com>
2019-03-23 15:46:48 -0700

    chore: Add eslint, prettier & husky

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    "@typescript-eslint/explicit-member-accessibility": false
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};
