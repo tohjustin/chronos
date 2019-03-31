ce4c8102d353ba3ca6af496b517ca5ef1ae55268
Justin Toh <tohjustin@hotmail.com>
2019-03-31 13:05:35 -0700

    feat(web-extensions/windows): Add FirefoxWindowsService

---
+++ src/web-extensions/windows/firefox.ts
@@ -0,0 +1,32 @@
+import {
+  FirefoxWindowsAPI,
+  BrowserWindowFocusChangedEvent,
+  BrowserWindowFocusChangedEventCallback,
+  WindowsService,
+  BrowserWindow
+} from "./types";
+
+/** The `windowId` value that represents the absence of a browser window. */
+export const FIREFOX_WINDOW_ID_NONE = browser.windows.WINDOW_ID_NONE;
+
+export class FirefoxWindowsService implements WindowsService {
+  private windows: FirefoxWindowsAPI;
+
+  public onFocusChanged: BrowserWindowFocusChangedEvent;
+
+  constructor(windows: FirefoxWindowsAPI = browser.windows) {
+    this.windows = windows;
+    this.onFocusChanged = {
+      addListener(callback: BrowserWindowFocusChangedEventCallback): void {
+        windows.onFocusChanged.addListener(callback);
+      },
+      removeListener(callback: BrowserWindowFocusChangedEventCallback): void {
+        windows.onFocusChanged.removeListener(callback);
+      }
+    };
+  }
+
+  get(windowId: number): Promise<BrowserWindow> {
+    return this.windows.get(windowId, { populate: true });
+  }
+}
---
+++ src/web-extensions/windows/index.ts
@@ -0,0 +1,29 @@
+import { FIREFOX_WINDOW_ID_NONE, FirefoxWindowsService } from "./firefox";
+import { WindowsService } from "./types";
+
+function getWindowIdNone(): number {
+  switch (process.env.REACT_APP_BUILD_TARGET) {
+    case "firefox":
+      return FIREFOX_WINDOW_ID_NONE;
+    default:
+      return -1;
+  }
+}
+
+/** The `windowId` value that represents the absence of a browser window. */
+export const WINDOW_ID_NONE = getWindowIdNone();
+
+/**
+ * Initialize service for interacting with browser windows
+ *
+ * @returns `WindowsService` object or `undefined` if the browser does not
+ * support interactions with browser windows.
+ */
+export function InitWindowsService(): WindowsService | undefined {
+  switch (process.env.REACT_APP_BUILD_TARGET) {
+    case "firefox":
+      return new FirefoxWindowsService();
+    default:
+      return undefined;
+  }
+}
