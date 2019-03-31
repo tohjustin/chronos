bca9650d518b241d14b1586d515e26d62b8608fe
Justin Toh <tohjustin@hotmail.com>
2019-03-31 13:33:53 -0700

    feat(web-extensions/windows): Add ChromeWindowsService

---
+++ src/web-extensions/windows/chrome.ts
@@ -0,0 +1,36 @@
+import {
+  ChromeWindowsAPI,
+  BrowserWindowFocusChangedEvent,
+  BrowserWindowFocusChangedEventCallback,
+  WindowsService,
+  BrowserWindow
+} from "./types";
+
+/** The `windowId` value that represents the absence of a browser window. */
+export const CHROME_WINDOW_ID_NONE = chrome.windows.WINDOW_ID_NONE;
+
+export class ChromeWindowsService implements WindowsService {
+  private windows: ChromeWindowsAPI;
+
+  public onFocusChanged: BrowserWindowFocusChangedEvent;
+
+  constructor(windows: ChromeWindowsAPI = chrome.windows) {
+    this.windows = windows;
+    this.onFocusChanged = {
+      addListener(callback: BrowserWindowFocusChangedEventCallback): void {
+        windows.onFocusChanged.addListener(callback);
+      },
+      removeListener(callback: BrowserWindowFocusChangedEventCallback): void {
+        windows.onFocusChanged.removeListener(callback);
+      }
+    };
+  }
+
+  get(windowId: number): Promise<BrowserWindow> {
+    return new Promise(resolve => {
+      this.windows.get(windowId, { populate: true }, window => {
+        resolve(window);
+      });
+    });
+  }
+}
--- src/web-extensions/windows/index.ts
+++ src/web-extensions/windows/index.ts
@@ -1,8 +1,11 @@
+import { CHROME_WINDOW_ID_NONE, ChromeWindowsService } from "./chrome";
 import { FIREFOX_WINDOW_ID_NONE, FirefoxWindowsService } from "./firefox";
 import { WindowsService } from "./types";
 
 function getWindowIdNone(): number {
   switch (process.env.REACT_APP_BUILD_TARGET) {
+    case "chrome":
+      return CHROME_WINDOW_ID_NONE;
     case "firefox":
       return FIREFOX_WINDOW_ID_NONE;
     default:
@@ -21,6 +24,8 @@ export const WINDOW_ID_NONE = getWindowIdNone();
  */
 export function InitWindowsService(): WindowsService | undefined {
   switch (process.env.REACT_APP_BUILD_TARGET) {
+    case "chrome":
+      return new ChromeWindowsService();
     case "firefox":
       return new FirefoxWindowsService();
     default:
