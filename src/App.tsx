47480a52f59dc14c3e2f6667a1fc268bc79e6d5a
Justin Toh <tohjustin@hotmail.com>
2019-05-14 19:19:13 +0800

    chore(App): Convert component to SFC

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ConnectedRouter } from "connected-react-router";
-import React, { Component } from "react";
+import React from "react";
 import { BarChart2, Settings } from "react-feather";
 import { hot } from "react-hot-loader";
 import { Provider } from "react-redux";
@@ -11,43 +11,39 @@ import store, { history } from "./store";
 import AnalyticsView from "./views/analytics";
 import SettingsView from "./views/settings";
 
-class App extends Component<{}, {}> {
-  render() {
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <HashRouter basename="">
-            <div className="app-container">
-              <Navbar
-                primaryItems={[
-                  {
-                    featherIcon: BarChart2,
-                    text: "Analytics",
-                    to: "/analytics"
-                  }
-                ]}
-                secondaryItems={[
-                  {
-                    featherIcon: Settings,
-                    text: "Settings",
-                    to: "/settings"
-                  }
-                ]}
-              />
-              <div className="view-container">
-                <Switch>
-                  <Route path="/analytics" component={AnalyticsView} />
-                  <Route path="/settings" component={SettingsView} />
-                  <Redirect exact to="/analytics" />
-                </Switch>
-              </div>
-            </div>
-          </HashRouter>
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <HashRouter basename="">
+        <div className="app-container">
+          <Navbar
+            primaryItems={[
+              {
+                featherIcon: BarChart2,
+                text: "Analytics",
+                to: "/analytics"
+              }
+            ]}
+            secondaryItems={[
+              {
+                featherIcon: Settings,
+                text: "Settings",
+                to: "/settings"
+              }
+            ]}
+          />
+          <div className="view-container">
+            <Switch>
+              <Route path="/analytics" component={AnalyticsView} />
+              <Route path="/settings" component={SettingsView} />
+              <Redirect exact to="/analytics" />
+            </Switch>
+          </div>
+        </div>
+      </HashRouter>
+    </ConnectedRouter>
+  </Provider>
+);
 
 export default (process.env.NODE_ENV === "development"
   ? hot(module)(App)
