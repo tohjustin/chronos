620d09fc1c522f7c9f7696a63d84f44e1aa60179
Justin Toh <tohjustin@hotmail.com>
2019-05-27 23:28:39 -0700

    infra: Add action & state sanitizers for Redux DevTools
    Currently indexedDB logged about 18k activity records, this causes our
    Redux state & actions to be too large for Redux DevTools to work
    properly so action + state sanitizers are required.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,42 @@
 import { routerMiddleware } from "connected-react-router";
 import { createHashHistory } from "history";
+import _ from "lodash";
 import { createStore, applyMiddleware, Middleware } from "redux";
 import { composeWithDevTools } from "remote-redux-devtools";
 import thunk from "redux-thunk";
 
 import createRootReducer from "./root-reducer";
+import { RootAction, RootState } from "./types";
 
 // configure middlewares
 export const history = createHashHistory();
 const middlewares: Middleware[] = [routerMiddleware(history), thunk];
 // compose enhancers
 const composeEnhancers = composeWithDevTools({
+  port: 8098,
   realtime: true,
-  port: 8098
+  actionSanitizer: (action: RootAction) => {
+    const sanitizedAction = _.cloneDeep(action);
+
+    if (action.type === "LOAD_ACTIVITY_SUCCESS" && action.payload) {
+      const recordCount = action.payload.length;
+      const recordsPlaceholder = `<<LOTS_OF_RECORDS_${recordCount}>>`;
+      _.set(sanitizedAction, "payload", recordsPlaceholder);
+    }
+
+    return sanitizedAction;
+  },
+  stateSanitizer: (state: RootState) => {
+    const sanitizedState = _.cloneDeep(state);
+
+    if (state.activity && state.activity.records) {
+      const recordCount = state.activity.records.length;
+      const recordsPlaceholder = `<<LOTS_OF_RECORDS_${recordCount}>>`;
+      _.set(sanitizedState, "activity.records", recordsPlaceholder);
+    }
+
+    return sanitizedState;
+  }
 });
 const enhancer = composeEnhancers(applyMiddleware(...middlewares));
 
