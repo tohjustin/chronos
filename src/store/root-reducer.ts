a04b8408088852d8e2d9de3ad4ec80765f996fec
Justin Toh <tohjustin@hotmail.com>
2019-05-28 20:24:08 -0700

    chore(store): Remove eslint-disable statement

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { connectRouter } from "connected-react-router";
 import { History } from "history";
 import { combineReducers } from "redux";
@@ -7,7 +5,7 @@ import { combineReducers } from "redux";
 import { reducer as activityReducer } from "./activity";
 import { reducer as dataMigrationReducer } from "./dataMigration";
 
-const createRootReducer = (history: History<any>) =>
+const createRootReducer = (history: History) =>
   combineReducers({
     activity: activityReducer,
     dataMigration: dataMigrationReducer,
