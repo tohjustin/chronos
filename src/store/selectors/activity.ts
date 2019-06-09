f334912bd46416fe6ee8a379f8f1ad6ab35d358f
Justin Toh <tohjustin@hotmail.com>
2019-06-08 22:39:21 -0700

    refactor(store/selectors): Revert back recent changes due to degraded perf
    Turns out the new FP approach in our selectors made the data
    computation slower by a factor of ~37.5, causing the app to freeze for
    ~1.5s when we change the time range to 'All' 🤦

--- src/store/selectors/activity.ts
+++ src/store/selectors/activity.ts
@@ -1,11 +1,8 @@
-import _ from "lodash";
 import moment from "moment";
 import { createSelector } from "reselect";
 
-import { ActivityRecord } from "../../db/types";
 import { DefiniteTimeRange } from "../../models/time";
 import { RootState } from "../../store/types";
-import { createActivitySplittingReducer } from "../../utils/activityUtils";
 import {
   getDateInMs,
   getDayOfWeekCount,
@@ -19,17 +16,6 @@ const DEFAULT_TIME_RANGE = {
     .valueOf(),
   end: null
 };
-
-const EMPTY_HOUR_OF_WEEK_DATA = (() => {
-  const result: { [hourOfWeekKey: string]: ActivityRecord[] } = {};
-  for (let day = 0; day < 7; day++) {
-    for (let hour = 0; hour < 24; hour++) {
-      const hourOfWeekKey = `${day},${hour}`;
-      result[hourOfWeekKey] = [];
-    }
-  }
-  return result;
-})();
 
 /**
  * Retrieves all activity records
@@ -133,34 +119,98 @@ export const getRecords = createSelector(
 export const getAverageDurationByHourOfWeek = createSelector(
   [getRecords, getEffectiveTimeRange],
   (records, effectiveTimeRange) => {
+    const avgDurationByHourOfWeek: { [hourOfWeek: string]: number } = {};
+    const totalDurationByHourOfWeek: {
+      [hourOfWeek: string]: { [date: string]: number };
+    } = {};
     const minDate = getDateInMs(effectiveTimeRange.start);
     const maxDate = getDateInMs(effectiveTimeRange.end);
 
-    return _.chain(records)
-      .reduce(createActivitySplittingReducer("hour"), [])
-      .filter(
-        r =>
-          r.startTime >= effectiveTimeRange.start &&
-          r.endTime <= effectiveTimeRange.end
-      )
-      .groupBy(record => {
-        const hourOfWeek = getHourOfWeek(record.startTime);
-        return `${hourOfWeek.day},${hourOfWeek.hour}`;
-      })
-      .merge(EMPTY_HOUR_OF_WEEK_DATA)
-      .mapValues((records: ActivityRecord[], hourOfWeekKey: string) => {
-        const [dayOfWeek, hourOfDay] = hourOfWeekKey.split(",").map(Number);
-        const dayOfWeekCount = getDayOfWeekCount(dayOfWeek, minDate, maxDate);
-        const totalDuration = _.sumBy(records, r => r.endTime - r.startTime);
+    records.forEach(record => {
+      let { startTime, endTime } = record;
+      let startHourOfWeek = getHourOfWeek(startTime);
+      let endHourOfWeek = getHourOfWeek(endTime);
+
+      // Handle records spanning over different hour of the week
+      while (
+        startHourOfWeek.day !== endHourOfWeek.day ||
+        startHourOfWeek.hour !== endHourOfWeek.hour
+      ) {
+        const newEndTime = new Date(endTime).setMinutes(0, 0, 0) - 1;
+        const newEndHourOfWeek = getHourOfWeek(newEndTime);
+
+        if (
+          getDateInMs(endTime) >= minDate &&
+          getDateInMs(endTime) <= maxDate
+        ) {
+          const hourOfWeekKey = String([endHourOfWeek.day, endHourOfWeek.hour]);
+          if (totalDurationByHourOfWeek[hourOfWeekKey] === undefined) {
+            totalDurationByHourOfWeek[hourOfWeekKey] = {};
+          }
+          const duration = endTime - newEndTime;
+          const prevTotalDuration =
+            totalDurationByHourOfWeek[hourOfWeekKey][getDateInMs(endTime)] || 0;
+          totalDurationByHourOfWeek[hourOfWeekKey][getDateInMs(endTime)] =
+            prevTotalDuration + duration;
+        }
+
+        [endTime, endHourOfWeek] = [newEndTime, newEndHourOfWeek];
+      }
+
+      // Compute total duration
+      if (
+        getDateInMs(startTime) >= minDate &&
+        getDateInMs(startTime) <= maxDate
+      ) {
+        const hourOfWeekKey = `${endHourOfWeek.day},${endHourOfWeek.hour}`;
+        const duration = endTime - startTime;
+        if (totalDurationByHourOfWeek[hourOfWeekKey] === undefined) {
+          totalDurationByHourOfWeek[hourOfWeekKey] = {};
+        }
+        const prevTotalDuration =
+          totalDurationByHourOfWeek[hourOfWeekKey][getDateInMs(endTime)] || 0;
+        totalDurationByHourOfWeek[hourOfWeekKey][getDateInMs(endTime)] =
+          prevTotalDuration + duration;
+      }
+    });
+
+    // Compute average duration
+    const hourOfWeekKeys = Object.keys(totalDurationByHourOfWeek);
+    for (let i = 0; i < hourOfWeekKeys.length; i++) {
+      const hourOfWeekKey = hourOfWeekKeys[i];
+      const dayOfWeek = Number(hourOfWeekKey.split(",")[0]);
+      const dates = Object.values(totalDurationByHourOfWeek[hourOfWeekKey]);
+
+      const totalDuration = dates.reduce((acc, val) => acc + val, 0);
+      const dayOfWeekCount = getDayOfWeekCount(dayOfWeek, minDate, maxDate);
+
+      avgDurationByHourOfWeek[hourOfWeekKey] =
+        totalDuration / Math.max(dayOfWeekCount, 1);
+    }
+
+    // Manually zero out hour-of-week with no activity
+    for (let day = 0; day < 7; day++) {
+      for (let hour = 0; hour < 24; hour++) {
+        const hourOfWeekKey = `${day},${hour}`;
+        if (avgDurationByHourOfWeek[hourOfWeekKey] === undefined) {
+          avgDurationByHourOfWeek[hourOfWeekKey] = 0;
+        }
+      }
+    }
+
+    // Sort results by chronological order
+    return Object.entries(avgDurationByHourOfWeek)
+      .map(([key, value]) => {
+        const [day, hour] = key.split(",");
         return {
-          day: dayOfWeek,
-          hour: hourOfDay,
-          duration: totalDuration / Math.max(dayOfWeekCount, 1)
+          day: Number(day),
+          hour: Number(hour),
+          duration: value
         };
       })
-      .values()
-      .sort((a, b) => (a.day * 24 + a.hour < b.day * 24 + b.hour ? -1 : 1))
-      .value();
+      .sort((a, b) => {
+        return a.day * 24 + a.hour < b.day * 24 + b.hour ? -1 : 1;
+      });
   }
 );
 
@@ -171,35 +221,62 @@ export const getAverageDurationByHourOfWeek = createSelector(
 export const getTotalDurationByDate = createSelector(
   [getRecords, getEffectiveTimeRange],
   (records, effectiveTimeRange) => {
+    const totalDurationByDate: { [timestamp: string]: number } = {};
     const minDate = getDateInMs(effectiveTimeRange.start);
     const maxDate = getDateInMs(effectiveTimeRange.end);
 
-    return _.chain(records)
-      .reduce(createActivitySplittingReducer("day"), [])
-      .filter(
-        r =>
-          r.startTime >= effectiveTimeRange.start &&
-          r.endTime <= effectiveTimeRange.end
-      )
-      .groupBy(record => getDateInMs(record.startTime))
-      .merge(
-        _.range(minDate, maxDate, 24 * 60 * 60 * 1000).reduce<{
-          [dateInMs: number]: ActivityRecord[];
-        }>((acc, dateInMs) => {
-          acc[dateInMs] = [];
-          return acc;
-        }, {})
-      )
-      .mapValues((records: ActivityRecord[], dateInMs: string) => {
-        const totalDuration = _.sumBy(records, r => r.endTime - r.startTime);
-        return {
-          timestamp: Number(dateInMs),
-          totalDuration
-        };
-      })
-      .values()
-      .sort((a, b) => (a.timestamp < b.timestamp ? -1 : 1))
-      .value();
+    records.forEach(record => {
+      let { startTime, endTime } = record;
+      let [startDate, endDate] = [getDateInMs(startTime), getDateInMs(endTime)];
+
+      // Handle records spanning over different dates
+      while (startDate !== endDate) {
+        const newEndTime = getDateInMs(endTime) - 1;
+        const newEndDate = getDateInMs(newEndTime);
+
+        if (endDate >= minDate && endDate <= maxDate) {
+          const duration = endTime - newEndTime;
+          const prevTotalDuration = totalDurationByDate[endDate] || 0;
+          totalDurationByDate[endDate] = prevTotalDuration + duration;
+        }
+
+        [endTime, endDate] = [newEndTime, newEndDate];
+      }
+
+      if (endDate >= minDate && endDate <= maxDate) {
+        const duration = endTime - startTime;
+        const prevTotalDuration = totalDurationByDate[endDate] || 0;
+        totalDurationByDate[endDate] = prevTotalDuration + duration;
+      }
+    });
+
+    let currentDate = minDate;
+    while (currentDate < maxDate) {
+      // Manually zero out days with no activity
+      if (totalDurationByDate[currentDate] === undefined) {
+        totalDurationByDate[currentDate] = 0;
+      }
+
+      // Limit usage time up to maximum value of 24 hours
+      if (totalDurationByDate[currentDate] > 0) {
+        totalDurationByDate[currentDate] = Math.min(
+          1000 * 60 * 60 * 24,
+          totalDurationByDate[currentDate]
+        );
+      }
+
+      currentDate += 1000 * 60 * 60 * 24;
+    }
+
+    // Sort results by chronological order
+    return Object.entries(totalDurationByDate)
+      .map(([key, value]) => ({
+        timestamp: Number(key),
+        totalDuration: value
+      }))
+      .sort((a, b) => {
+        return a.timestamp < b.timestamp ? -1 : 1;
+      });
   }
 );
 
