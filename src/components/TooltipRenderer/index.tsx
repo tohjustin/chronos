b8d1b72aab794e960c5f2ae95d719a3576ae1ef2
Justin Toh <tohjustin@hotmail.com>
2019-07-21 23:38:23 -0700

    feat(components/TooltipRenderer): Add TooltipRenderer component

---
+++ src/components/TooltipRenderer/index.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+import { useClientDimensions } from "../../hooks";
+
+import "./styles.scss";
+
+interface TooltipProps<T> {
+  className: string;
+  data: T | null;
+}
+
+interface TooltipRendererProps<T> {
+  data: T | null;
+  height: number;
+  width: number;
+  isHovering: boolean;
+  marginX: number;
+  marginY: number;
+  offsetX: number;
+  offsetY: number;
+  x: number | null;
+  y: number | null;
+
+  component?: React.FC<TooltipProps<T>>;
+}
+
+function DefaultTooltip<T>(props: TooltipProps<T>) {
+  return <div className={props.className}>{JSON.stringify(props.data)}</div>;
+}
+
+function TooltipRenderer<T>(props: TooltipRendererProps<T>) {
+  const [tooltipRef, tooltipHeight, tooltipWidth] = useClientDimensions();
+
+  // Compute tooltip positions
+  let tooltipX = 0;
+  let tooltipY = 0;
+  if (props.x && props.y) {
+    tooltipX =
+      props.offsetX + props.x + tooltipWidth < props.width
+        ? props.x
+        : props.x - tooltipWidth;
+    tooltipY =
+      props.offsetY + props.y - tooltipHeight > 0
+        ? props.y - tooltipHeight
+        : props.y;
+  }
+  const Tooltip = props.component || DefaultTooltip;
+
+  return (
+    <div
+      ref={tooltipRef}
+      className="tooltip__container"
+      style={{
+        position: "absolute",
+        left: `${tooltipX}px`,
+        top: `${tooltipY}px`,
+        padding: `${props.marginY}px ${props.marginX}px`,
+        opacity: props.isHovering ? 1 : 0,
+        pointerEvents: "none"
+      }}
+    >
+      <Tooltip className="tooltip" data={props.data} />
+    </div>
+  );
+}
+
+export default TooltipRenderer;
