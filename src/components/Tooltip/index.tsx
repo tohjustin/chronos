1cc232114ee7a753d8880e1b98c5008f3cd0ca7d
Justin Toh <tohjustin@hotmail.com>
2019-07-26 22:06:21 -0700

    feat(components/Tooltip): Add generic Tooltip component

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface TooltipProps {
+  header: string;
+  body: React.ReactNode;
+  headerImageAltText?: string;
+  headerImageUrl?: string;
+}
+
+const Tooltip = (props: TooltipProps) => {
+  return (
+    <div className="tooltip">
+      <div className="tooltip__header">
+        {props.headerImageUrl && (
+          <img alt={props.headerImageAltText} src={props.headerImageUrl} />
+        )}
+        <strong>{props.header}</strong>
+      </div>
+      <div className="tooltip__body">{props.body}</div>
+    </div>
+  );
+};
+
+export default Tooltip;
