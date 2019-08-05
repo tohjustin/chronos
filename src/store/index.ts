f1eda7fe279e08fa7a237c7be29d025e41fc02b0
Justin Toh <tohjustin@hotmail.com>
2019-08-04 23:28:45 -0700

    infra(package.json): Add REACT_APP_DEBUG_MODE env to enable DevTools

--- src/store/index.ts
+++ src/store/index.ts
@@ -12,43 +12,43 @@ import { RootAction, RootState } from "./types";
 export const history = createHashHistory();
 const middlewares = [routerMiddleware(history), thunk];
 
-// compose enhancers (typed as `any` b/c `composeWithDevTools` doesn't work
-// nicely with Redux on Typescript 😭 This shouldn't affect the typechecking
-// for our redux state, action & reducers though...)
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const composeEnhancers: any = composeWithDevTools({
-  port: 8098,
-  realtime: true,
-  actionSanitizer: (action: RootAction) => {
-    const sanitizedAction = _.cloneDeep(action);
+let enhancer = applyMiddleware(...middlewares);
+if (process.env.REACT_APP_DEBUG_MODE) {
+  // compose enhancers (typed as `any` b/c `composeWithDevTools` doesn't work
+  // nicely with Redux on Typescript 😭 This shouldn't affect the type-checking
+  // for our redux state, action & reducers though...)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const composeEnhancers: any = composeWithDevTools({
+    port: 8098,
+    realtime: true,
+    actionSanitizer: (action: RootAction) => {
+      const sanitizedAction = _.cloneDeep(action);
 
-    if (action.type === "LOAD_ACTIVITY_SUCCESS" && action.payload) {
-      const recordCount = action.payload.length;
-      const recordsPlaceholder = `<<LOTS_OF_RECORDS_${recordCount}>>`;
-      _.set(sanitizedAction, "payload", recordsPlaceholder);
+      if (action.type === "LOAD_ACTIVITY_SUCCESS" && action.payload) {
+        const recordCount = action.payload.length;
+        const recordsPlaceholder = `<<LOTS_OF_RECORDS_${recordCount}>>`;
+        _.set(sanitizedAction, "payload", recordsPlaceholder);
+      }
+
+      return sanitizedAction;
+    },
+    stateSanitizer: (state: RootState) => {
+      const sanitizedState = _.cloneDeep(state);
+
+      if (state.activity && state.activity.records) {
+        const recordCount = state.activity.records.length;
+        const recordsPlaceholder = `<<LOTS_OF_RECORDS_${recordCount}>>`;
+        _.set(sanitizedState, "activity.records", recordsPlaceholder);
+      }
+
+      return sanitizedState;
     }
+  });
+  enhancer = composeEnhancers(enhancer);
+}
 
-    return sanitizedAction;
-  },
-  stateSanitizer: (state: RootState) => {
-    const sanitizedState = _.cloneDeep(state);
-
-    if (state.activity && state.activity.records) {
-      const recordCount = state.activity.records.length;
-      const recordsPlaceholder = `<<LOTS_OF_RECORDS_${recordCount}>>`;
-      _.set(sanitizedState, "activity.records", recordsPlaceholder);
-    }
-
-    return sanitizedState;
-  }
-});
-const enhancer = composeEnhancers(applyMiddleware(...middlewares));
-
-// rehydrate state on app start
-const initialState = {};
-
-// create store
-const store = createStore(createRootReducer(history), initialState, enhancer);
+const INITIAL_STATE = {};
+const store = createStore(createRootReducer(history), INITIAL_STATE, enhancer);
 
 // export store singleton instance
 export default store;
