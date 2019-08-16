dbd4d427525543f8b29e0c7ba1bfb567afc38716
Justin Toh <tohjustin@hotmail.com>
2019-08-15 23:46:44 -0700

    refactor(components/CountUp): Make transition duration unit changes smoother

--- src/components/CountUp/index.tsx
+++ src/components/CountUp/index.tsx
@@ -32,37 +32,66 @@ const MS_PER_SECOND = 1000;
 const MS_PER_MINUTE = 60 * 1000;
 const MS_PER_HOUR = 60 * 60 * 1000;
 
-const formatMinutes = (value: number) => `${value}`.padStart(2, "0");
+const formatHour = (value: number) => {
+  return value < MS_PER_HOUR ? "" : `${Math.floor(value / MS_PER_HOUR)}`;
+};
+const formatMinutes = (value: number) => {
+  if (value < MS_PER_MINUTE) {
+    return "";
+  }
+  const hours = Math.floor(value / MS_PER_HOUR);
+  const minutes = Math.floor((value % MS_PER_HOUR) / MS_PER_MINUTE);
+  return hours === 0 ? `${minutes}` : `${minutes}`.padStart(2, "0");
+};
+const formatSeconds = (value: number) => {
+  return value > MS_PER_MINUTE || value < MS_PER_SECOND
+    ? ""
+    : `${Math.floor(value / MS_PER_SECOND)}`;
+};
+const formatMilliseconds = (value: number) => {
+  return value > MS_PER_SECOND ? "" : `${value}`;
+};
+const formatUnits = (
+  value: number,
+  unit?: string,
+  formattingFn?: (value: number) => string
+): string => {
+  if (
+    formattingFn === undefined ||
+    unit === undefined ||
+    formattingFn(value) === ""
+  ) {
+    return "";
+  }
 
-export const CountUp = (props: CountUpProps) => (
-  <>
-    <ReactCountUp className="count-up__value" {...props} end={props.end} />
-    {props.unit && <span className="count-up__unit">{props.unit}</span>}
-  </>
-);
+  return unit;
+};
 
-export const DurationCountUp = (props: DurationCountUpProps) => {
-  const hours = Math.floor(props.end / MS_PER_HOUR);
-  const minutes = Math.floor((props.end % MS_PER_HOUR) / MS_PER_MINUTE);
-  const seconds =
-    hours > 0 || minutes > 0
-      ? undefined
-      : Math.floor(props.end / MS_PER_SECOND);
-  const milliseconds = props.end >= MS_PER_SECOND ? undefined : props.end;
+export const CountUp = ({ unit, ...props }: CountUpProps) => {
+  const { formattingFn } = props;
+  const formattingUnitFn = React.useMemo(() => {
+    return (value: number) => formatUnits(value, unit, formattingFn);
+  }, [formattingFn, unit]);
 
   return (
+    <>
+      <ReactCountUp className="count-up__value" {...props} />
+      <ReactCountUp
+        className="count-up__unit"
+        {...props}
+        formattingFn={formattingUnitFn}
+      />
+    </>
+  );
+};
+
+export const DurationCountUp = (props: DurationCountUpProps) => {
+  return (
     <div className="duration-count-up">
-      {hours > 0 && <CountUp {...props} end={hours} unit="h" />}
-      {minutes > 0 && (
-        <CountUp
-          {...props}
-          end={minutes}
-          formattingFn={formatMinutes}
-          unit="min"
-        />
-      )}
-      {seconds && <CountUp {...props} end={seconds} unit="s" />}
-      {milliseconds && <CountUp {...props} end={milliseconds} unit="ms" />}
+      <CountUp {...props} formattingFn={formatHour} unit="h" />
+      <CountUp {...props} formattingFn={formatMinutes} unit="min" />
+      <CountUp {...props} formattingFn={formatSeconds} unit="s" />
+      <CountUp {...props} formattingFn={formatMilliseconds} unit="ms" />
     </div>
   );
 };
