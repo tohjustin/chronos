c0c5e82e40bf74bc959bcfbab73e8d56fe64b52a
Justin Toh <tohjustin@hotmail.com>
2019-08-24 22:10:46 -0700

    feat(components/LineChart): Support transition animation

--- src/components/LineChart/Chart.tsx
+++ src/components/LineChart/Chart.tsx
@@ -1,5 +1,8 @@
 import * as d3 from "d3";
+import { interpolatePath } from "d3-interpolate-path";
 import React, { useEffect, useRef } from "react";
+
+import { usePrevious } from "../../hooks";
 
 import { Datum } from "./types";
 
@@ -17,6 +20,10 @@ interface ChartProps {
   transitionDelay: number;
 }
 
+function resetData(data: Datum[]) {
+  return data.map(d => ({ ...d, y: 0 }));
+}
+
 const Chart = ({
   data,
   height,
@@ -30,7 +37,9 @@ const Chart = ({
   transitionDelay
 }: ChartProps) => {
   const chartRef = useRef<SVGGElement | null>(null);
-
+  const prevData = usePrevious(data);
+  const prevScaleX = usePrevious(scaleX) || scaleX;
+  const prevScaleY = usePrevious(scaleY) || scaleY;
   useEffect(() => {
     const svg = d3.select(chartRef.current);
 
@@ -62,23 +71,51 @@ const Chart = ({
       .line<Datum>()
       .x(d => scaleX(d.x))
       .y(d => scaleY(d.y));
+    const prevLineGenerator = d3
+      .line<Datum>()
+      .x(d => prevScaleX(d.x))
+      .y(d => prevScaleY(d.y));
+    const initialData = prevData || resetData(data);
+    const isTransitioningFromALine = initialData.length > 1;
+    const isTransitioningIntoALine = data.length > 1;
     svg
       .append("path")
-      .datum(data)
+      .datum(initialData)
       .attr("class", "line-chart__line")
-      .attr("d", lineGenerator);
+      .attr("d", prevLineGenerator)
+      .transition()
+      .attrTween("d", function() {
+        const current = isTransitioningFromALine
+          ? d3.select(this).attr("d")
+          : lineGenerator(resetData(data));
+        const next = isTransitioningIntoALine
+          ? lineGenerator(data)
+          : prevLineGenerator(resetData(initialData));
+        return interpolatePath(current || "", next || "");
+      })
+      .style("opacity", data.length > 1 ? 1 : 0)
+      .duration(transitionDelay);
 
     // Draw single dot if there's only a single datapoint
-    if (data.length === 1) {
-      svg
-        .selectAll(".dot")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("class", "line-chart__dot")
-        .attr("cx", d => scaleX(d.x))
-        .attr("cy", d => scaleY(d.y));
-    }
+    const isTransitioningFromASingleDot = prevData && prevData.length === 1;
+    const isTransitioningIntoASingleDot = data.length === 1;
+    svg
+      .selectAll(".line-chart__dot")
+      .data(data.length === 0 ? initialData : [data[0]])
+      .enter()
+      .append("circle")
+      .attr("class", "line-chart__dot")
+      .style("opacity", isTransitioningFromASingleDot ? 1 : 0)
+      .attr("cx", () => width / 2)
+      .attr("cy", () =>
+        prevData && isTransitioningFromASingleDot
+          ? prevScaleY(prevData[0].y)
+          : height
+      )
+      .transition()
+      .attr("cy", d => (isTransitioningIntoASingleDot ? scaleY(d.y) : height))
+      .style("opacity", isTransitioningIntoASingleDot ? 1 : 0)
+      .duration(transitionDelay);
   }, [
     data,
     height,
@@ -86,8 +123,12 @@ const Chart = ({
     onMouseEnter,
     onMouseLeave,
     onMouseOver,
+    prevData,
+    prevScaleX,
+    prevScaleY,
     scaleX,
     scaleY,
+    transitionDelay,
     width
   ]);
 
