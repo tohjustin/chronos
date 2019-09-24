55e9c54f6e980b2bff9a8bae0df8befdbbd0c815
Justin Toh <tohjustin@hotmail.com>
2019-09-23 22:30:48 -0700

    style(view/analytics): Fix DomainPicker header position

--- src/styles/constants.ts
+++ src/styles/constants.ts
@@ -1,7 +1,9 @@
-const BASE_SIZE = 8;
+export const BASE_SIZE = 8;
 
 export const BUTTON_SIZE = BASE_SIZE * 4;
 export const BUTTON_MARGIN = BASE_SIZE * 1.5;
+
+export const ICON_BUTTON_SIZE = BASE_SIZE * 3;
 
 export const ICON_SIZE = BASE_SIZE * 2.5;
 
