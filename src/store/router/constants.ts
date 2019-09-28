34ead8806894db84493c4db4722d2c92734db96c
Justin Toh <tohjustin@hotmail.com>
2019-09-27 21:14:02 -0700

    fix(store/router): Fix default time range value

--- src/store/router/constants.ts
+++ src/store/router/constants.ts
@@ -1,13 +1,14 @@
 import { getStartOfDay } from "../../utils/dateUtils";
 
-const MS_PER_WEEK = 7 * 24 * 60 * 60 * 1000;
-
 // Domain
 export const SEARCH_PARAM_DOMAIN = "domain";
 
 // Time Range
+const MS_PER_DAY = 24 * 60 * 60 * 1000;
+const MS_PER_WEEK = MS_PER_DAY * 7;
+const DEFAULT_START_TIME_OFFSET = 4 * MS_PER_WEEK - MS_PER_DAY;
 export const DEFAULT_TIME_RANGE = {
-  start: getStartOfDay(Date.now() - (4 * MS_PER_WEEK - 1)),
+  start: getStartOfDay(Date.now()) - DEFAULT_START_TIME_OFFSET,
   end: null
 };
 export const SEARCH_PARAM_START_DATE = "startDate";
