e1ed0b500da3d5e03027de21692452149e4779c7
Justin Toh <tohjustin@hotmail.com>
2019-12-01 15:20:24 -0800

    feat(views/settings): Add confirmation dialog when importing data

--- src/views/settings/ExtensionDataCard.tsx
+++ src/views/settings/ExtensionDataCard.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
 import { Button } from "../../components/Button";
 import Card from "../../components/Card";
+import Dialog from "../../components/Dialog";
 import FileUploadButton from "../../components/FileUploadButton";
 import { List, ListItem } from "../../components/List";
 import { useStorageEstimate } from "../../hooks";
@@ -15,28 +16,43 @@ import { formatBytes, formatDateDistance } from "./utils";
 interface ExtensionDataCardProps {
   exportDatabaseRecords: () => void;
   importDatabaseRecords: (data: string) => void;
-  isLoadingRecords: boolean;
   activityTimeRange: DefiniteTimeRange | null;
+  isLoadingRecords: boolean;
 }
 
-const ExtensionDataCard = (props: ExtensionDataCardProps) => {
+const ExtensionDataCard = ({
+  activityTimeRange,
+  exportDatabaseRecords,
+  importDatabaseRecords,
+  isLoadingRecords
+}: ExtensionDataCardProps) => {
+  const [importDataFile, setImportDataFile] = useState<File | null>(null);
+  const [showConfirmImportDialog, setShowConfirmImportDialog] = useState<
+    boolean
+  >(false);
   const [storageUsage, storageQuota] = useStorageEstimate();
 
-  function handleFileUploadChange(e: React.ChangeEvent<HTMLInputElement>) {
-    if (e.target.files) {
+  const handleFileUploadChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
       const [uploadedFile] = e.target.files;
+      setImportDataFile(uploadedFile);
+      setShowConfirmImportDialog(true);
+    }
+  };
+  const handleImportDataConfirm = (close: () => void) => {
+    close();
+    if (importDataFile) {
       const reader = new FileReader();
 
       reader.onload = () => {
         const fileContent = reader.result;
-        if (typeof fileContent !== "string") {
-          return;
+        if (typeof fileContent === "string") {
+          importDatabaseRecords(fileContent);
         }
-        props.importDatabaseRecords(fileContent);
       };
-      reader.readAsText(uploadedFile);
+      reader.readAsText(importDataFile);
     }
-  }
+  };
 
   return (
     <Card
@@ -44,27 +60,44 @@ const ExtensionDataCard = (props: ExtensionDataCardProps) => {
       title="Extension Data"
       description="Manage data collected & stored by the extension"
       body={
-        <List className="settings-view__list">
-          <ListItem
-            label="Storage data used"
-            isLoading={props.isLoadingRecords}
-            value={`${formatBytes(storageUsage)} / ${formatBytes(
-              storageQuota
-            )}`}
-          />
-          <ListItem
-            label="Total data collected"
-            isLoading={props.isLoadingRecords}
-            value={
-              props.activityTimeRange
-                ? formatDateDistance(
-                    props.activityTimeRange.start,
-                    props.activityTimeRange.end
-                  )
-                : "-"
-            }
-          />
-        </List>
+        <>
+          <List className="settings-view__list">
+            <ListItem
+              label="Storage data used"
+              isLoading={isLoadingRecords}
+              value={`${formatBytes(storageUsage)} / ${formatBytes(
+                storageQuota
+              )}`}
+            />
+            <ListItem
+              label="Total data collected"
+              isLoading={isLoadingRecords}
+              value={
+                activityTimeRange
+                  ? formatDateDistance(
+                      activityTimeRange.start,
+                      activityTimeRange.end
+                    )
+                  : "-"
+              }
+            />
+          </List>
+          <Dialog
+            confirmLabel="Proceed & Overwrite Data"
+            hasClose={false}
+            intent="danger"
+            isShown={showConfirmImportDialog}
+            onConfirm={handleImportDataConfirm}
+            onCloseComplete={() => {
+              setShowConfirmImportDialog(false);
+              setImportDataFile(null);
+            }}
+            title="Import Data"
+          >
+            This action will overwrite existing application data with the
+            imported data. Do you still want to proceed?
+          </Dialog>
+        </>
       }
       footer={
         <div className="settings-view__button-panel">
@@ -74,7 +107,7 @@ const ExtensionDataCard = (props: ExtensionDataCardProps) => {
           <Button
             appearance="primary"
             iconBefore="export"
-            onClick={props.exportDatabaseRecords}
+            onClick={exportDatabaseRecords}
           >
             Export Data
           </Button>
@@ -98,7 +131,4 @@ const mapDispatchToProps = (dispatch: Dispatch) =>
     dispatch
   );
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ExtensionDataCard);
+export default connect(mapStateToProps, mapDispatchToProps)(ExtensionDataCard);
