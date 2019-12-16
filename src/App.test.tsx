d6349171bb222d6b5126cfc99b18d014e55c9c20
Justin Toh <tohjustin@hotmail.com>
2019-12-15 17:49:12 -0800

    test(App): Refactor to use testing-library/react

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
+import { render } from "@testing-library/react";
 import React from "react";
-import ReactDOM from "react-dom";
 
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("renders without crashing", () => {
+  const { getByText } = render(<App />);
+  const headerElement = getByText(/Usage Analytics/i);
+
+  expect(headerElement).toBeInTheDocument();
 });
