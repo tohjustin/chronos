f619fb5776db9eb36c1e327e3ddb464df8cb06ea
Justin Toh <tohjustin@hotmail.com>
2020-02-20 21:38:07 -0800

    infra: Fix generation of output zip file
    - Update zip process to copy over Webpack build directory content
      instead of copying & creating a "build" folder inside the zip file.
    - Upgrade `adm-zip` to latest version (v0.4.14) to get CRC fix in
      https://github.com/cthackers/adm-zip/pull/267

--- pack-extension.js
+++ pack-extension.js
@@ -5,17 +5,19 @@ const path = require("path");
 const GIT_COMMIT_SHA = process.env.REACT_APP_GIT_COMMIT_SHA;
 const WEBPACK_BUILD_DIR = "./build";
 
+// Prune & rename extension manifest file
 fs.removeSync(`${WEBPACK_BUILD_DIR}/manifest.development.json`);
 fs.renameSync(
   `${WEBPACK_BUILD_DIR}/manifest.production.json`,
   `${WEBPACK_BUILD_DIR}/manifest.json`
 );
 
+// Generate output file name
 let outputFilename = path.basename(WEBPACK_BUILD_DIR);
 if (GIT_COMMIT_SHA) {
   outputFilename += `-${GIT_COMMIT_SHA}`;
 }
 
 const zip = new AdmZip();
-zip.addLocalFolder(WEBPACK_BUILD_DIR);
+zip.addLocalFolder(WEBPACK_BUILD_DIR, "");
 zip.writeZip(`${outputFilename}.zip`);
